42bbcb1a771a0c485c5e79378399012dbe8de99c
ferrc897 <ferrodriguez8972005@gmail.com>
2024-10-18 16:44:50 -0600

    Most functions implemented

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -49,28 +49,57 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    row, column = action
+    if action not in actions(board):
+        raise Exception("Invalid Move")
+
+    result = board
+    result[row][column] = player(board)
+
+    return result
+    
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
-
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] is not EMPTY:
+            return board[i][0]
+    for j in range(len(board)):
+        if board[0][j] == board[1][j] == board[2][j] and board[0][j] is not EMPTY:
+            return board[i][j]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not EMPTY:
+        return board[0][2]
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    count = 0
+    if winner(board) is not None:
+        return True
+    if actions(board) is None:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    result = winner(board)
+
+    if result == X:
+        return 1
+    elif result == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
